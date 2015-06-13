2ba44328366a0454d891d3930d060d98b34a1f54
Dennis Schulz <dennis@port42.org>
2015-06-13 20:58:10 +0200

    moves sqlite3 gem to development and testing only. adds a DOCKER_OPTIONS_RUN file that defines that the ALSA sound device sound be available within a docker container of the soundbaord. modifies sounds controller to handle mplayer without a terminal like inside a docker container when not run binded to the terminal in non-interactive mode.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -70,10 +70,10 @@ class SoundsController < ApplicationController
       if os == :macosx
         player_bin = "afplay"
       else
-        player_bin = "mplayer -really-quiet"
+        player_bin = "mplayer -really-quiet -ao alsa"
       end
 
-      play_command = "#{player_bin} #{@sound.soundfile.path}"
+      play_command = "nohup #{player_bin} #{@sound.soundfile.path} &> /dev/null < /dev/null"
 
       Rails.logger.info play_command
       system play_command
