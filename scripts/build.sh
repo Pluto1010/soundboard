891ee4a76ea951a466cceb72d565e4d3a54c9e63
Dennis Schulz <dennis@port42.org>
2015-06-17 13:02:59 +0200

    changed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,10 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 cd ../frontend
-npm install -g grunt-cli &&
 npm install -g bower &&
 npm install &&
+npm install -g grunt-cli &&
+
+
 bower --allow-root install &&
 grunt production build --force
