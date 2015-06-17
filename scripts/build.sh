5fc8650eda9ba5147d598505c64f5553875760e4
Dennis Schulz <dennis@port42.org>
2015-06-17 13:10:32 +0200

    emilio changed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,23 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 cd ../frontend
-npm install -g bower &&
-npm install &&
+echo "###############################" &&
+echo "INSTALLING GRUNT-CLI" &&
+echo "###############################" &&
 npm install -g grunt-cli &&
-
-
+echo "###############################" &&
+echo "INSTALLING BOWER" &&
+echo "###############################" &&
+npm install -g bower &&
+echo "###############################" &&
+echo "RUNNING NPM INSTALL" &&
+echo "###############################" &&
+npm install &&
+echo "###############################" &&
+echo "RUNNING BOWER INSTALL" &&
+echo "###############################" &&
 bower --allow-root install &&
+echo "###############################" &&
+echo "BUILDING, TEHEE" &&
+echo "###############################" &&
 grunt production build --force
