7cf8642d186555ba2f4022be1dc9f8ab699caf98
Dennis Schulz <dennis@port42.org>
2015-06-18 15:27:56 +0200

    adds basic auth to soundboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: "noise", password: "sndbrd!" #, except: :index
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
